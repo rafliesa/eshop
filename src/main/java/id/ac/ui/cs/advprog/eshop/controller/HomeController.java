7ce79a2ae36710bf568c847722b4860a8f6fd11a
rafliesa <rflisaa@gmail.com>
2025-02-13 20:01:35 +0700

    feat: created home

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
+}
